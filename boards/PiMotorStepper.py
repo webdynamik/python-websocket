398e91249fb7b809a1f0f049ae180aab02d89499
jan <jan@webdynamik.de>
2020-04-26 18:02:21 +0200

    init

---
+++ boards/PiMotorStepper.py
@@ -0,0 +1,8 @@
+import PiMotor
+import time
+import RPi.GPIO as GPIO
+
+m1 = PiMotor.Stepper("STEPPER1")
+
+def forward():
+	m1.forward(0.05,10)  # Delay and rotations
---
+++ client.py
@@ -0,0 +1,45 @@
+import socketio
+import RPi.GPIO as GPIO
+from boards.PiMotorStepper import * as PiMotorStepper
+
+try:
+    sio = socketio.Client()
+
+    @sio.on('connect')
+    def on_connect():
+        print('connection established')
+        sio.emit('hallo', 'PiMotorStepper')
+        sio.emit('add-devices', [{
+                                    "name": "SMARS",
+                                    "type": "smars",
+                                    "ip": "x",
+                                    "location": '',
+                                    "component": "smarsComponent"
+                                  }])
+        print('--')
+
+    @sio.on('display')
+    def on_message(data):
+        print('received: ', data)
+
+    @sio.on('get')
+    def get(data):
+        #fn({status: 1, value: 1, index: data.index})
+        print('get: ', data)
+
+    @sio.on('SmarsSet')
+    def SmarsSet(data):
+        print('set: ', data)
+        exec('PiMotorStepper.' + data+'()');
+        
+    @sio.on('disconnect')
+    def on_disconnect():
+        print('disconnected from server')
+
+    sio.connect('http://192.168.178.29:8888')
+    sio.wait()
+except KeyboardInterrupt:
+        stop()
+        GPIO.cleanup();
+        print('bye bye')
+
