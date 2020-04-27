677fc3827170e791c41ef71b7c35275bebaacf13
jan <jan@webdynamik.de>
2020-04-27 19:34:11 +0200

    motor 2

--- commands/PiMotorStepper.py
+++ commands/PiMotorStepper.py
@@ -3,9 +3,12 @@ import time
 import RPi.GPIO as GPIO
 
 m1 = PiMotor.Stepper("STEPPER1")
+m2 = PiMotor.Stepper("STEPPER2")
 
 def forward(delay, rotations):
 	m1.forward(delay, rotations)
+	m2.forward(delay, rotations)
 
 def backward(delay, rotations):
 	m1.backward(delay, rotations)
+	m2.backward(delay, rotations)
