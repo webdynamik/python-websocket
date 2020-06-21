9a8fadc5a8217db32041c4ac9be39e562065da34
jan <jan@webdynamik.de>
2020-06-21 13:47:05 +0200

    s

--- commands/PiMiniMotor.py
+++ commands/PiMiniMotor.py
@@ -2,16 +2,17 @@ import RPi.GPIO as GPIO
 import time
 servoPIN = 21
 GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50)
 
 def penOff():
-  p.start(8)
+  p = GPIO.PWM(servoPIN, 50)
+  p.start(10)
   p.ChangeDutyCycle(8)
   p.stop()
   time.sleep(0.5)
 
 def penOn():
-  p.start(10)
+  p = GPIO.PWM(servoPIN, 50)
+  p.start(8)
   p.ChangeDutyCycle(10)
   p.stop()
   time.sleep(0.5)
