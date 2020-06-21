c8ba5456e9b8123365de43f0f133cd034a11b666
jan <jan@webdynamik.de>
2020-06-21 14:27:34 +0200

    as

--- commands/penOn.py
+++ commands/penOn.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 servoPIN = 21
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
