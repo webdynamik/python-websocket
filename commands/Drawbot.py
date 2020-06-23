57ee1b199d965702b4779ad7230247acb281e896
jan <jan@webdynamik.de>
2020-06-23 22:01:19 +0200

    directions

--- commands/Drawbot.py
+++ commands/Drawbot.py
@@ -5,25 +5,29 @@ m1 = PiMotor.Stepper("STEPPER1")
 m2 = PiMotor.Stepper("STEPPER2")
 
 def left(delay, rotations):
-	m1.forward(delay, rotations)
+	m1.backward(delay, rotations)
+	m2.forward(delay, rotations)
 
 def right(delay, rotations):
 	m1.forward(delay, rotations)
+	m2.backward(delay, rotations)
 
 def top(delay, rotations):
-	m1.forward(delay, rotations)
+	m1.backward(delay, rotations)
+	m2.backward(delay, rotations)
 
 def down(delay, rotations):
 	m1.forward(delay, rotations)
+	m2.forward(delay, rotations)
 
 def leftTop(delay, rotations):
-	m1.forward(delay, rotations)
+	m1.backward(delay, rotations)
 
 def rightTop(delay, rotations):
-	m1.forward(delay, rotations)
+	m2.backward(delay, rotations)
 
 def leftDown(delay, rotations):
-	m1.forward(delay, rotations)
+	m2.forward(delay, rotations)
 
 def rightDown(delay, rotations):
 	m1.forward(delay, rotations)
