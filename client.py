d10866bf37a037101c96f20e375740d0fd02226f
jan <jan@webdynamik.de>
2020-06-23 20:37:19 +0200

    drawbot

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import config
-import socketio
-from commands.PiMotorStepper import m1Stop as PiMotorStepperStop, m2Stop as PiMotorStepper2Stop, m1Forward as PiMotorStepperForward, m1Backward as PiMotorStepperBackward, m2Forward as PiMotorStepper2Forward, m2Backward as PiMotorStepper2Backward
+import socketio from commands.Drawbot import stop as DrawbotStop, left as DrawbotLeft, right as DrawbotRight, top as DrawbotTop, down as DrawbotDown, leftTop as DrawbotLeftTop, rightTop as DrawbotRightTop, leftDown as DrawbotLeftDown, rightDown as DrawbotRightDown
 import os
 
 try:
@@ -9,13 +8,10 @@ try:
     @sio.on('connect')
     def on_connect():
         print('connection established')
-        print(config.motors)
         sio.emit('setOnline', config.motors)
 
     @sio.on('sayHello')
     def on_say_hello():
-        print('on_say_hello')
-        print(config.motors)
         sio.emit('setOnline', config.motors)
 
     @sio.on('pen')
@@ -28,24 +24,30 @@ try:
 
     @sio.on('stop')
     def stop(data):
-        print('-> stop', data)
-        if data['motor'] == '1' :
-            PiMotorStepperStop();
-        else:
-            PiMotorStepper2Stop();
+        print('-> stop')
+        DrawbotStop();
 
     @sio.on('motor')
     def Motor(data):
         print('set Motor to: ', data)
 
-        if data['motor'] == '2' and data['direction'] == 'forward' :
-            PiMotorStepper2Forward(data['delay'],data['rotations']);
-        elif data['motor'] == '2' and data['direction'] == 'backward' :
-            PiMotorStepper2Backward(data['delay'],data['rotations']);
-        elif data['motor'] == '1' and data['direction'] == 'backward' :
-            PiMotorStepperBackward(data['delay'],data['rotations']);
-        else: 
-            PiMotorStepperForward(data['delay'],data['rotations']);
+        if data['direction'] == 'down' :
+            DrawbotDown(data['delay'],data['rotations']);
+        elif data['direction'] == 'left' :
+            DrawbotLeft(data['delay'],data['rotations']);
+        elif data['direction'] == 'leftDown' :
+            DrawbotLeftDown(data['delay'],data['rotations']);
+        elif data['direction'] == 'leftTop' :
+            DrawbotLeftTop(data['delay'],data['rotations']);
+        elif data['direction'] == 'right' :
+            DrawbotRight(data['delay'],data['rotations']);
+        elif data['direction'] == 'rightDown' :
+            DrawbotRightDown(data['delay'],data['rotations']);
+        elif data['direction'] == 'rightTop' :
+            DrawbotRightTop(data['delay'],data['rotations']);
+        elif data['direction'] == 'top' :
+            DrawbotTop(data['delay'],data['rotations']);
+
         
     @sio.on('disconnect')
     def on_disconnect():
---
+++ commands/Drawbot.py
@@ -0,0 +1,33 @@
+import boards.PiMotor as PiMotor
+import time
+
+m1 = PiMotor.Stepper("STEPPER1")
+m2 = PiMotor.Stepper("STEPPER2")
+
+def left(delay, rotations):
+	m1.forward(delay, rotations)
+
+def right(delay, rotations):
+	m1.forward(delay, rotations)
+
+def top(delay, rotations):
+	m1.forward(delay, rotations)
+
+def down(delay, rotations):
+	m1.forward(delay, rotations)
+
+def leftTop(delay, rotations):
+	m1.forward(delay, rotations)
+
+def rightTop(delay, rotations):
+	m1.forward(delay, rotations)
+
+def leftDown(delay, rotations):
+	m1.forward(delay, rotations)
+
+def rightDown(delay, rotations):
+	m1.forward(delay, rotations)
+
+def stop():
+	m1.stop()
+	m2.stop()
